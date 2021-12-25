56824f181e8fa33544d761f18a8e1d24dc972556
tray190 <belltracy411@gmail.com>
2021-12-25 11:59:27 -0600

    Linq

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var games = new List<string>() { "street fighter, days gone, assasin creed, spiderman, GTA, Far Cry 6" };
+
+            games.OrderBy(x => x.Length).ToList().ForEach(x => System.Console.WriteLine(x));
+
+        }
+    }
+}
